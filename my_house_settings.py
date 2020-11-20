a0747c49200abab14aa1dbefe40de22f2353b910
Martin Parker <48888522+RasPiPkr@users.noreply.github.com>
2020-11-20 16:46:36 +0000

    Updated for CCTV

--- my_house_settings.py
+++ my_house_settings.py
@@ -1,10 +1,24 @@
 # Settings for my Heating & Security Control
 
+# CCTV variables
+user = '' # In the string put your user name for your cctv system
+passwd = '' # In the string put your password for you cctv system
+camIP = '' # In the string put your local IP address for your cctv system
+httpPort = '' # In the string enter your http port setup that is set in your cctv system
+cam1 = '101' # Dependng on your cctv system for how many cameras or how the web request is required to view said camera
+cam2 = '201' # Replace these values, I have left them in as these worked with my cctv system
+cam3 = '301' # For my system the first digit refers to the camera and the 01 being the main stream 02 being substream
+# Main stream is for best quality
+
 # Google voice announcements
 dialog = [] # Put links to a host for a recorded mp3, I used dropbox for mine
 
 # Testing google home mini
 kitchen_google = '' # In the string enter your local IP address for your google for sound from
+
+# Default heating timer settings, these are defaults for when the GUI starts
+morningTimer = '07:00:00' # Set them in here or when the GUI is running you can change them
+bedtimeTimer = '23:00:00'
 
 # ZONES
 # ZONE DEFAULT OPTIONS: 0 = off, 1 = auto, 2 = on (This sets the GUI for how it works
